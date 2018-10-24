0b67613a41b710e1b9f86dd2f79557df9297fa9f
saturnaut (from Travis CI) <saturnaut@gmail.com>
2018-10-24 08:29:21 +0000

    Deploy saturnaut/saturnaut.github.io to github.com/saturnaut/saturnaut.github.io.git:master

---
+++ precache-manifest.b632ee22828faf0134478cfd39aa361e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e6e5381f86e63a686d2",
+    "url": "/saturnaut/saturnaut.github.io/static/css/main.abe139a8.chunk.css"
+  },
+  {
+    "revision": "7e6e5381f86e63a686d2",
+    "url": "/saturnaut/saturnaut.github.io/static/js/main.7e6e5381.chunk.js"
+  },
+  {
+    "revision": "fdbcf8e1c41f1dba5da7",
+    "url": "/saturnaut/saturnaut.github.io/static/js/1.fdbcf8e1.chunk.js"
+  },
+  {
+    "revision": "adbb4afdf7af4f066a7b",
+    "url": "/saturnaut/saturnaut.github.io/static/js/runtime~main.adbb4afd.js"
+  },
+  {
+    "revision": "a85a3c9cf17518a5cf0937520d2d2255",
+    "url": "/saturnaut/saturnaut.github.io/index.html"
+  }
+];
--- src/state/reducers/root.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import app from 'state/reducers/app';
-
-export default combineReducers({
-  app,
-});
