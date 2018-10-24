5aba1224f30dd39de45df670ae3b143b339247f4
saturnaut (from Travis CI) <saturnaut@gmail.com>
2018-10-24 08:53:41 +0000

    Deploy saturnaut/saturnaut.github.io to github.com/saturnaut/saturnaut.github.io.git:master

---
+++ precache-manifest.e3bf6ea26b6ec6b26b714c7ca2294168.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e6e5381f86e63a686d2",
+    "url": "/static/css/main.abe139a8.chunk.css"
+  },
+  {
+    "revision": "7e6e5381f86e63a686d2",
+    "url": "/static/js/main.7e6e5381.chunk.js"
+  },
+  {
+    "revision": "fdbcf8e1c41f1dba5da7",
+    "url": "/static/js/1.fdbcf8e1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d16e310d1d6b8551b5afbd8bf6ab8a2d",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/saturnaut/saturnaut.github.io/precache-manifest.b632ee22828faf0134478cfd39aa361e.js"
+  "/precache-manifest.e3bf6ea26b6ec6b26b714c7ca2294168.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/saturnaut/saturnaut.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
